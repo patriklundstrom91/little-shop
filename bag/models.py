34dea81c851c496315ba119d5282fd96387c8be3
patriklundstrom91 <patriklundstrom91@gmail.com>
2025-07-15 17:10:01 +0200

    added apps bag, orders and profiles. Added profiles models and signals

--- bag/models.py
+++ bag/models.py
@@ -1,3 +1,18 @@
+from django.conf import settings
 from django.db import models
+from shop.models import ProductVariant
 
 # Create your models here.
+
+
+class BagItem(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,
+                             blank=True, on_delete=models.CASCADE)
+    session_key = models.CharField(max_length=50, blank=True)
+    variant = models.ForeignKey(ProductVariant, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+
+    class Meta:
+        unique_together = ('user', 'session_key', 'variant')
+
+
---
+++ profiles/models.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django_countries.fields import CountryField
+
+# Create your models here.
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=100, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    address = models.CharField(max_length=250, blank=True, null=True)
+    city = models.CharField(max_length=100, blank=True, null=True)
+    postal_code = models.CharField(max_length=20, blank=True)
+    state_province = models.CharField(max_length=100, blank=True, null=True)
+    country = CountryField(blank_label='Country', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+
