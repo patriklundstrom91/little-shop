f31682fd325560b251c0395941a17e547a048c14
patriklundstrom91 <patriklundstrom91@gmail.com>
2025-07-18 13:43:50 +0200

    added order history on my profile page

--- profiles/views.py
+++ profiles/views.py
@@ -3,16 +3,19 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .models import UserProfile
 from .forms import UserProfileForm
+from orders.models import Order
 
 # Create your views here.
 
 
 @login_required
 def view_profile(request):
-    """ Display user profile """
+    """ Display user profile and order history"""
     profile, _ = UserProfile.objects.get_or_create(user=request.user)
+    orders = Order.objects.filter(user=profile.user).order_by('-created')
     return render(
-        request, 'profiles/view_profile.html', {'profile': profile}
+        request, 'profiles/view_profile.html', {'profile': profile,
+                                                'orders': orders}
     )
 
 
