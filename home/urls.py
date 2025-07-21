03306ffd3df27f8ff58aba83d3dbc9226f311c57
patriklundstrom91 <patriklundstrom91@gmail.com>
2025-07-21 03:12:37 +0200

    added global color and font to css and restyled index

--- home/urls.py
+++ home/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -5,3 +7,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
--- profiles/urls.py
+++ profiles/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from . import views
@@ -10,3 +12,9 @@ urlpatterns = [
     path('delete/', views.delete_profile, name='delete_profile'),
     path('orders/<int:order_id>/', views.order_detail, name='order_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
