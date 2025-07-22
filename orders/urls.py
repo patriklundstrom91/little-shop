300fe8b39b64dbd707bd516faea439ebdbfd49ea
patriklundstrom91 <patriklundstrom91@gmail.com>
2025-07-22 12:00:42 +0200

    minor fix

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import stripe_webhook
+
 
 app_name = 'orders'
 
@@ -9,7 +9,7 @@ urlpatterns = [
     path('checkout/', views.checkout_view, name='checkout'),
     path('start/', views.create_checkout_session,
          name='create_checkout_session'),
-    path('webhook/', stripe_webhook, name='stripe_webhook'),
+    path('webhook/', views.stripe_webhook, name='stripe_webhook'),
     path('success/', views.checkout_success, name='checkout_success'),
     path('cancel/', views.checkout_cancel, name='checkout_cancel'),
     path('order/<int:order_id>/mark_delivered/', views.mark_order_delivered,
